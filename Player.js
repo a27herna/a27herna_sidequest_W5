7112b64e3765ff3034ea16fd13670cc5239aadea
HoiyaNaranja <a27herna@uwaterloo.ca>
2026-02-19 15:27:41 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -40,6 +40,6 @@ This keeps it flexible.
 */
 function overlapAABB(a, b) {
   return (
-    a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y
+    a.x <= b.x + b.w && a.x + a.w >= b.x && a.y <= b.y + b.h && a.y + a.h >= b.y
   );
 }
